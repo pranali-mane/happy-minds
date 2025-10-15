90bd398f08c6b4699ba8749cdfbea25d2f4f1cbd
Pranali Mane <141844394+pranali-mane@users.noreply.github.com>
2025-10-15 23:14:02 +0530

    Add files via upload
    Initial commit — Happy Minds Mental Health Website

---
+++ mindex.php
@@ -0,0 +1,175 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Happy Minds</title>
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    
+<!-- header section starts  -->
+
+<header class="header">
+    
+    <a href="index.php" class="logo"><img src="images/feel-well.png" width= 60px></a>
+
+    <nav class="navbar">
+        <a href="index.php">home</a>
+        <a href="blog.html">blog</a>
+        <a href="booksession.html">Therapists</a>
+        <a href="">Online Counselling</a>
+        <a href="happyspace.html">Therapies</a>
+        <a href="quiza.html">Self Assesment</a>
+        <a href="signUp.php">Sign Up</a>
+        <a href="">Login</a>
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+</header>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+
+    <div class="content">
+        <h3>Happy Minds</h3>
+        <p>An all-in-one mental well being platform.</p>
+    </div>
+
+    <div class="image">
+        <img src="images/welcome.png" alt="welcome">
+    </div>
+
+    <div class="cloud cloud-1"></div>
+    <div class="cloud cloud-2"></div>
+
+</section>
+
+<!-- home section ends -->
+
+<!-- contact section starts  -->
+
+<center><section class="space" id="space">
+
+    <h1 class="heading"> Happy <span>Minds</span>  </h1>
+
+    <div class="icons-container">
+        <div class="icons">
+            <a  href="booksession.html"><img src="images/individualC.jpg" alt="" ><h1>Individual Councelling</h1></a>
+        </div>
+        <div class="icons">
+            <a  href="booksession.html"><img src="images/teen.webp" alt="" >
+        <h1>Teen Therapy</h1></a>
+
+        </div>
+        <div class="icons">
+            <a  href=""><img src="images/ESP.png" alt="" >
+        <h1>Employees assistance program</h1></a>
+        </div>
+        <div class="icons">
+            <a  href=""><img src="images/campus.jpg" alt="" >
+        <h1>Campus wellness program</h1></a>
+        </div>
+    </div>
+
+
+</section>
+<center>
+
+<!-- contact section ends -->
+
+
+<!-- about section starts  -->
+
+<section class="about" id="about">
+
+    <h1 class="heading"> <span>about</span> us </h1>
+    
+    <div class="row">
+
+        <div class="image">
+            <img src="images/about-img.png" alt="">
+        </div>
+
+        <div class="content">
+            <h3 class="title">Who Are We?</h3>
+            <p>A group of young minds, who care about your mental well being. </p>
+            <a href="" class="btn">learn more about us</a>
+            <div class="icons-container">
+                <div class="icons">
+                    <a href="happyspace.html"><i class="fas fa-award"></i></a>
+                    <h3>Mental Health Awareness</h3>
+                    <p>We wish to create a safe and a happy space for people suffering from mental health problems and also spread awareness along our way.</p>
+                </div>
+                <div class="icons">
+                    <i class="fas fa-user"></i>
+                    <h3>Satisfied Visitors</h3>
+                    <p>Our visitors are our family and we wish nothing but the best for them. We have created a happy space for everyone out there and have received tremendous amount of appreciation and love from those who visited the website.</p>
+                </div>
+                <div class="icons">
+                    <a href="blog.html"><i class="fas fa-project-diagram"></i></a>
+                    <h3>Informative Blogs</h3>
+                    <p>Apart from the services provided by the Feel Well Web App, there also exists a section dedicated to informative and interesting blogs related mental health.</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- about section ends -->
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+    <div class="box-container">
+        
+
+        <div class="box" style="padding-left: 10%;">
+            <h3>quick links</h3>
+            <a href="#"> <i class="fas fa-arrow-right"></i> home </a>
+            <a href="#"> <i class="fas fa-arrow-right"></i> about </a>
+            <a href="#"> <i class="fas fa-arrow-right"></i> contact </a>
+        </div>
+
+        <div class="box" style="padding-left: 60%;">
+            <h3>Follow Us</h3>
+            <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+            <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+            <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+        </div>
+
+    </div>
+
+    <div class="credit">created by <span>Feel Well Team 
+        ❤️</span> | all rights reserved</div>
+
+</section>
+
+<!-- footer section ends -->
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+<script src="https://cdn.botpress.cloud/webchat/v1/inject.js"></script>
+<script src="https://mediafiles.botpress.cloud/6139f4e2-c1a4-4176-bd7a-e3239cad49e5/webchat/config.js" defer></script>
+</body>
+</html>
+<?php
+?>
---
+++ signUp.html
@@ -0,0 +1,44 @@
+<?php
+// import user object
+require_once('core/classes/user.Class.php');
+
+// assig User object to $User variable
+$User = new User;
+
+// getting the action
+$a = isset($_GET['a']) ? $_GET['a'] : '';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login/Register User</title>
+    <link rel='stylesheet' href='css/Signup.css' />
+</head>
+<body>
+<?php
+switch($a){
+    case 'login': {
+        echo $User -> LoginForm();
+        break;
+    }
+    case 'register': {
+        echo $User -> RegisterForm();
+        break;
+    }
+    case 'dashboard': {
+        if($User -> Is_Login())
+            echo $User->Dashboard();
+        else
+            echo "Session expired!";
+        break;
+    }
+    default: {
+        echo $User -> LoginForm();
+        break;
+    }
+}
+?>
+</body>
+</html>
